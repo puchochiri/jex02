a9edcf8bba5157648245e13d275b8fa18d2ce71b
양선우 <@DESKTOP-94O1RKP>
2022-09-07 16:31:19 +0900

    commit_2022.09.07_paging기능 추가

--- jex02/src/test/java/org/zerock/mapper/BoardMapperTests.java
+++ jex02/src/test/java/org/zerock/mapper/BoardMapperTests.java
@@ -1,11 +1,14 @@
 package org.zerock.mapper;
 
+import java.util.List;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.zerock.domain.BoardVO;
+import org.zerock.domain.Criteria;
 
 import lombok.Setter;
 import lombok.extern.log4j.Log4j;
@@ -20,10 +23,10 @@ public class BoardMapperTests {
 	@Setter(onMethod_ = @Autowired)
 	private BoardMapper mapper;
 	
-	@Test
-	public void testGetList() {
-		mapper.getList().forEach(board -> log.info(board));
-	}
+	//@Test
+	//public void testGetList() {
+	//	mapper.getList().forEach(board -> log.info(board));
+	//}
 	
 	@Test
 	public void testInsert() {
@@ -74,6 +77,19 @@ public class BoardMapperTests {
 		log.info("UPDATE COUNT: " + count);
 	}
 	
+	@Test
+	public void testPaging() {
+		
+		log.info("testPaging");
+		Criteria cri = new Criteria();
+		//10개씩 3page
+		cri.setPageNum(3);
+		cri.setAmount(10);
+		
+		List<BoardVO> list = mapper.getListWithPaging(cri);
+		
+		list.forEach(board -> log.info(board));
+	}
 	
 
 }
